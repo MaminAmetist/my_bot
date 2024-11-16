613b644f2efdb29705218837db158f1fd0cc6fdf
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-11-16 14:41:43 +0700

    Delete module_14_5_reg.py

--- module_14_5_reg.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from crud_functions_14_4 import get_all_products, connection
-
